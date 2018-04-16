1cadd3eba39fa6c2e170ce0935a234f2e891655d
Hai Dang <danghuyen2504@gmai.com>
2018-04-16 16:33:52 +0700

    test homepage

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Expo from 'expo';
+import HomePage from './src/components/HomePage';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.ignoredYellowBox = ['Warning', 'Setting', 'Possible'];
+
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("native-base/Fonts/Ionicons.ttf"),
+    });
+    this.setState({isReady: true});
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    if (!this.state.isReady) {
+      return <Expo.AppLoading/>;
+    }
+    return <HomePage/>;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/HomePage.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import {Container,
+        Header,
+        Content,
+        View,
+        Text,
+        Item, 
+        Icon,
+        Input,
+        Footer,
+        FooterTab,
+        Button,
+        Card,
+        CardItem
+} from 'native-base';
+import { FlatList, TouchableOpacity } from 'react-native';
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { id: 1, value: 'JS' },
+                { id: 2, value: 'Android' },
+                { id: 3, value: 'iOS' },
+                { id: 4, value: 'C++' },
+                { id: 5, value: 'C#' },
+                { id: 6, value: 'PHP' },
+                { id: 7, value: 'Java' },
+                { id: 8, value: 'HTML' },
+                { id: 9, value: 'Python' },
+                { id: 10, value: 'HTML' },
+                { id: 11, value: 'CSS' },
+                { id: 12, value: 'Ruby' },
+                { id: 13, value: 'D' },
+                { id: 14, value: 'F' },
+                { id: 15, value: 'XML' },
+                { id: 16, value: 'JSON' },
+                { id: 17, value: 'VB' },
+                { id: 18, value: 'Haskell' },
+                { id: 19, value: 'BrainFuck' },
+                { id: 20, value: 'Pascal' },
+            ]
+        };
+    }
+
+    _renderItem = ({item}) => {
+        return (
+            <Button style = {{width: 100, height: 100, marginRight: 10, backgroundColor: 'lightblue'}}>
+                <Text>{item.value}</Text>
+            </Button>
+        );
+    }
+
+    render() {
+        return (
+            <Container>
+                <View style = {{height: 23, backgroundColor: '#00a574'}}/>
+                <Header searchBar rounded
+                        style = {{backgroundColor: '#00a574'}}>
+                    <Item>
+                        <Icon name = 'ios-search'/>
+                        <Input placeholder = 'Search'/>
+                    </Item>
+                </Header>
+                <Content padder>
+                    <Card>
+                        <CardItem>
+                            <View style = {{flexDirection: 'column', flex: 1}}>
+                                <View style = {{flexDirection: 'row', marginBottom: 10}}>
+                                    <Text style = {{flex: 1}}>Recommended</Text>
+                                    <TouchableOpacity>
+                                        <Text>MORE</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <FlatList   data = {this.state.data}
+                                            renderItem = {this._renderItem}
+                                            horizontal = {true}
+                                            keyExtractor = {item => item.id}>
+                                </FlatList>
+                            </View>
+                        </CardItem>
+                    </Card>
+                    <Card>
+                        <CardItem>
+                            <View style = {{flexDirection: 'column', flex: 1}}>
+                                <View style = {{flexDirection: 'row', marginBottom: 10}}>
+                                    <Text style = {{flex: 1}}>Recommended</Text>
+                                    <TouchableOpacity>
+                                        <Text>MORE</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <FlatList   data = {this.state.data}
+                                            renderItem = {this._renderItem}
+                                            horizontal = {true}
+                                            keyExtractor = {item => item.id}>
+                                </FlatList>
+                            </View>
+                        </CardItem>
+                    </Card>
+                </Content>
+                <Footer>
+                    <FooterTab style = {{backgroundColor: '#00a574'}}>
+                        <Button vertical transparent>
+                            <Icon name = 'ios-home-outline' style = {{color: '#fff'}}/>
+                            <Text style = {{color: '#fff'}}>Home</Text>
+                        </Button>
+                        <Button vertical transparent>
+                            <Icon name = 'ios-book-outline' style = {{color: '#fff'}}/>
+                            <Text style = {{color: '#fff'}}>Library</Text>
+                        </Button>
+                        <Button vertical transparent>
+                            <Icon name = 'ios-person-outline' style = {{color: '#fff'}}/>
+                            <Text style = {{color: '#fff'}}>User</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
+        );
+    }
+}
